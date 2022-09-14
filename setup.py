d89b10695a151c53b9b10de427df4b63b7573a8d
Rafael Corsi <corsiferrao@gmail.com>
2022-09-14 14:31:18 -0300

    add wheel

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="1.0",
     packages=["bits", "bits.util"],
     include_package_data=True,
-    install_requires=["click", "pyyaml", "pytest", "tabulate", "myhdl"],
+    install_requires=["click", "pyyaml", "pytest", "tabulate", "myhdl", "wheel"],
     entry_points="""
         [console_scripts]
         bits=bits:cli
